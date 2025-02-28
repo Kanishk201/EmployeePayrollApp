9d6255d6c58a254d48fec38204c64a6eec9dd890
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-28 14:47:58 +0530

    [Kanishk] Add. Use lombok library for logging

--- src/main/java/com/springboot/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/springboot/employeepayrollapp/dto/EmployeeDTO.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class EmployeeDTO {
     private String name;
     private Double salary;
